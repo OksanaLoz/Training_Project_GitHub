2c14c53f6f82f724cbc294739a0cd2ab2208c85f
Oksana L <oksana_loz@i.ua>
2023-11-07 13:40:39 +0200

    label axes, number of dots = 100, line symbol is *

--- plot_github.py
+++ plot_github.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pylab
 
-x=np.linspace(-1,1,1000)
+x=np.linspace(-1,1,100)
 y=(x)*np.sin(1/x)
-pylab.plot(x, y,'--r')
+pylab.plot(x, y,'*r')
+pylab.xlabel('x')
+pylab.ylabel('y')
