d0d045d704bb6d6037c4d08ff2de9a70e78f45a0
Oksana L <oksana_loz@i.ua>
2023-11-17 12:17:00 +0200

    round the number of years, remove useless lines

--- Money.py
+++ Money.py
@@ -1,13 +1,6 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Mar  6 16:46:43 2023
-
-@author: Dell
-"""
-
 from financial_functions import years
 
 P = 1; r = 5
-n = years(P, 2*P, r)
+n = round(years(P, 2*P, r))
 
 print(f'Money has doubled after {n} years')
